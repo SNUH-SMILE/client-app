7bf2d25264ef94ac5f40792b6e6a7d76a15b10d9
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:40:15 +0900

    공지사항 상세화면 컴포넌트 개발

---
+++ project/v2/src/components/NoticeDetail.vue
@@ -0,0 +1,19 @@
+<template>
+  <fragment>
+    <div class="noti-top">
+      <p class="ttl">[생활치료센터]자가 격리 물품 배부 안내</p>
+      <span class="date">등록 일자: 2022.01.01</span>
+    </div>
+    <div class="noti-btm">
+      <div class="pre-box">생활치료센터에서 알려드립니다. 자가 격리 물품을 배부해드립니다. 각 호실 문 앞으로 배부 예정입니다. 확인 부탁드립니다.</div>
+    </div>
+  </fragment>
+</template>
+
+<script>
+export default {
+  props: {},
+};
+</script>
+
+<style></style>
