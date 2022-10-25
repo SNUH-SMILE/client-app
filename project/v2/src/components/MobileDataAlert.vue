27e99890383e169c9a8a0483559399891860ec8d
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:39:05 +0900

    모바일 데이터 modal 컴포넌트 개발

---
+++ project/v2/src/components/MobileDataAlert.vue
@@ -0,0 +1,30 @@
+<template>
+  <modal-layout title="모바일 데이터">
+    <template slot="body">
+      <p class="pop-txt">
+        Wifi에 연결하지 않고 LTE/5G 이용 시
+        <br />데이터 요금이 부과될 수 있습니다. <br /><br />재생 하시겠습니까?
+      </p>
+    </template>
+    <template slot="foot">
+      <div class="btn-wrap">
+        <button type="button" class="btn-txt white" @click="$emit('onClose')">재생취소</button>
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'exercise-video' }">
+          <button type="button" class="btn-txt blue" @click="navigate">재생하기</button>
+        </router-link>
+      </div>
+    </template>
+    <template slot="noti">
+      <p class="ipt-chk-noti">
+        <input type="checkbox" id="chk01" />
+        <label for="chk01">다시 표시 안함</label>
+      </p>
+    </template>
+  </modal-layout>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
