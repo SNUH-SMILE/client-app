a79e711542533209112830339078163a37bce473
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:51:52 +0900

    loading 플러그인 추가

---
+++ project/v2/src/plugins/modal/components/Loading.vue
@@ -0,0 +1,18 @@
+<template>
+  <transition name="fade" appear v-if="show">
+    <div class="loading-wrap show">
+      <div class="loading">
+        <img src="@/assets/img/img-loading.png" alt="" />
+      </div>
+    </div>
+  </transition>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
