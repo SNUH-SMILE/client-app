2bcb4551b957f9c6b773906025bfe630d5f33ae2
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:41:48 +0900

    문진 항목 컴포넌트 개발

---
+++ project/v2/src/modules/history/components/VHistoryTakingItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="form-item">
+    <validation-provider mode="passive" :name="index" :rules="question.answerRequired" tag="fragment" v-slot="{ errors }">
+      <label class="form-ttl">{{ index }}. {{ question.question }}</label>
+      <component :is="question.answerType" :question="question" :value="value" @input="handleInput" />
+      <span>{{ errors[0] }}</span>
+    </validation-provider>
+  </div>
+</template>
+<script>
+import AnswerComponents from '@/modules/history/components/answercomponents';
+export default {
+  props: {
+    value: null,
+    index: Number,
+    question: Object,
+  },
+  data() {
+    return {};
+  },
+  components: { ...AnswerComponents },
+  methods: {
+    handleInput(value) {
+      this.$emit('input', value);
+    },
+  },
+};
+</script>
