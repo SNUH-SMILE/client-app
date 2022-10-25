465fb297ca4a7e560330f52e2ed232a3709ed13f
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 15:06:51 +0900

    약관 동의 페이지 개발

---
+++ project/v2/src/views/terms/_id.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="content-wrap">
+    <validation-observer tag="fragment">
+      <div class="content">
+        <div class="cont-inner mb-space30">
+          <div class="terms-wrap">약관 내용이 들어갑니다.</div>
+        </div>
+      </div>
+      <div class="btn-wrap">
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'agree' }">
+          <button type="button" class="btn-txt navy" @click="navigate">동의</button>
+        </router-link>
+      </div>
+    </validation-observer>
+  </div>
+</template>
+<route>
+{
+  "meta": {
+    "title": "IITP 자가 격리 앱 서비스 이용약관"
+  }
+}
+</route>
+<script>
+const INIT_STATE = () => ({});
+
+export default {
+  data() {
+    return {
+      state: INIT_STATE(),
+    };
+  },
+};
+</script>
+
+<style></style>
---
+++ project/v2/src/views/terms/index.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="content-wrap">
+    <validation-observer tag="fragment">
+      <div class="content">
+        <div class="cont-inner mb-space30">
+          <h1 class="ttl-l">
+            <strong>자가격리</strong> <span class="fw-100">APP</span>을 <br class="tb-none" />사용하기 전 <br />아래
+            <span class="fw-500 txtc-blue">약관에 동의</span>해 <br class="tb-none" />주세요.
+          </h1>
+          <div class="agree-wrap">
+            <p class="ipt-chk all-chk-box">
+              <input type="checkbox" id="allChk" />
+              <label for="allChk">전체 동의</label>
+            </p>
+            <ul class="agree-list">
+              <li>
+                <p class="ipt-chk">
+                  <input type="checkbox" id="chk01" />
+                  <label for="chk01" class="">IITP 자가 격리 앱 서비스 이용약관</label>
+                </p>
+                <router-link custom v-slot="{ navigate }" :to="{ name: 'terms-id' }">
+                  <button type="button" class="btn-ic-detail" @click="navigate">
+                    <span class="txt-blind">상세보기</span>
+                  </button>
+                </router-link>
+              </li>
+
+              <li>
+                <p class="ipt-chk">
+                  <input type="checkbox" id="chk02" />
+                  <label for="chk02" class="">개인정보 수집 및 이용 동의</label>
+                </p>
+                <router-link custom v-slot="{ navigate }" :to="{ name: 'terms-id' }">
+                  <button type="button" class="btn-ic-detail" @click="navigate">
+                    <span class="txt-blind">상세보기</span>
+                  </button>
+                </router-link>
+              </li>
+              <li>
+                <p class="ipt-chk">
+                  <input type="checkbox" id="chk03" />
+                  <label for="chk03" class="">개인정보 제3자 제공 동의</label>
+                </p>
+                <router-link custom v-slot="{ navigate }" :to="{ name: 'terms-id' }">
+                  <button type="button" class="btn-ic-detail" @click="navigate">
+                    <span class="txt-blind">상세보기</span>
+                  </button>
+                </router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+      <div class="btn-wrap">
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'target-check' }">
+          <button type="button" class="btn-txt navy" @click="navigate" disabled>다음</button>
+        </router-link>
+      </div>
+    </validation-observer>
+  </div>
+</template>
+<route>
+{
+  "meta": {
+    "title": "약관동의"
+  }
+}
+</route>
+<script>
+const INIT_STATE = () => ({});
+
+export default {
+  data() {
+    return {
+      state: INIT_STATE(),
+    };
+  },
+};
+</script>
+
+<style></style>
