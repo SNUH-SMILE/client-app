03036aa4b48497f36fc6cc5d725405f8eef900fd
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 10:45:02 +0900

    eslint 사용

---
+++ project/v2/.eslintrc.js
@@ -0,0 +1,26 @@
+module.exports = {
+  root: true,
+  env: {
+    node: true,
+  },
+  extends: [
+    "plugin:vue/essential",
+    "eslint:recommended",
+    "plugin:prettier/recommended",
+  ],
+  globals: {
+    _: 'readonly',
+    $: 'readonly',
+    M: 'readonly',
+  },
+  parserOptions: {
+    parser: "@babel/eslint-parser",
+  },
+  rules: {
+    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
+    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'vue/multi-word-component-names': 'off',
+    'no-useless-escape': 'off',
+    'no-unused-vars' : 'warn'
+  },
+};
