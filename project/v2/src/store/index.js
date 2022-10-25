513efc4c5932ab0538d4d6102df9a89703d1dbcf
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 15:01:10 +0900

    vue store 추가

---
+++ project/v2/src/store/index.js
@@ -0,0 +1,35 @@
+import Vue from 'vue';
+import Vuex from 'vuex';
+import _debounce from 'lodash/debounce';
+
+Vue.use(Vuex);
+
+const store = new Vuex.Store({
+  state: {
+    isMobile: document.documentElement.clientWidth < 768,
+  },
+  getters: {},
+  mutations: {
+    setIsMobile(state, payload) {
+      state.isMobile = payload;
+    },
+  },
+  actions: {},
+  modules: {},
+});
+
+const onChangeSize = _debounce(() => {
+  const width = document.documentElement.clientWidth;
+  if (width > 768 && store.state.isMobile) {
+    // table
+    store.commit('setIsMobile', false);
+  } else if (width < 768 && !store.state.isMobile) {
+    // mobile
+    store.commit('setIsMobile', true);
+  }
+}, 0);
+
+window.removeEventListener('resize', onChangeSize);
+window.addEventListener('resize', onChangeSize);
+
+export default store;
