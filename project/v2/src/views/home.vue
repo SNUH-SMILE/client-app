9fb70f45caf5e62b47afb280242364b4d0289ecc
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 10:29:08 +0900

    홈 화면 개발

---
+++ project/v2/src/views/home.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="content-wrap">
+    <home-contents />
+    <history-taking-alarm v-if="popup === 'alarm'" @onAction="onHtAlarmAction" />
+  </div>
+</template>
+<script>
+import HistoryTakingAlarm from '@/components/HistoryTakingAlarm.vue';
+import HomeContents from '@/components/HomeContents.vue';
+export default {
+  name: 'home',
+  components: {
+    HistoryTakingAlarm,
+    HomeContents,
+  },
+  data() {
+    return {
+      popup: '',
+    };
+  },
+  methods: {
+    onHtAlarmAction(action) {
+      this.popup = '';
+    },
+  },
+};
+</script>
+
+<style></style>
