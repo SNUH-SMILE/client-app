94379ad25d6d89e8f3939a38af7851ef8ab74c85
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 10:45:14 +0900

    바벨 사용

---
+++ project/v2/.prettierrc.js
@@ -0,0 +1,20 @@
+// prettier.config.js or .prettierrc.js
+module.exports = {
+  arrowParens: 'always',
+  bracketSpacing: true,
+  endOfLine: 'lf',
+  htmlWhitespaceSensitivity: 'css',
+  insertPragma: false,
+  jsxBracketSameLine: false,
+  jsxSingleQuote: false,
+  printWidth: 150,
+  proseWrap: 'preserve',
+  quoteProps: 'as-needed',
+  requirePragma: false,
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  trailingComma: 'es5',
+  useTabs: false,
+  vueIndentScriptAndStyle: false,
+};
