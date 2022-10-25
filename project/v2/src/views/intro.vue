8233b227796486f0737ec3fc058e537b84fb2172
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 10:33:20 +0900

    인트로 화면 기능 개발

---
+++ project/v2/src/views/intro.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="intro-wrap">
+    <div class="center-box">
+      <h1 class="logo-box">
+        <span class="logo">
+          <span class="txt-blind">SMILE</span>
+        </span>
+        <span class="app-txt"><strong>자가격리</strong> APP</span>
+      </h1>
+      <div class="loading">
+        <img src="@/assets/img/img-loading.png" alt="" />
+      </div>
+    </div>
+    <p class="ver-txt">v1.0</p>
+    <router-view></router-view>
+  </div>
+</template>
+<script>
+export default {
+  layout: 'none',
+  methods: {
+    haveAllPermission() {
+      //TODO: 위치, 마이크, 블루투스 권한
+      console.log('저는 위치, 마이크, 블루투스 권한을 가지고 있습니다.');
+      return true;
+    },
+    havePushPermission() {
+      //TODO : 체크하기
+      console.log('저는 푸시 권한을 가지고 있습니다.');
+      return true;
+    },
+  },
+  mounted() {
+    setTimeout(() => {
+      if (!this.haveAllPermission()) {
+        this.$router.replace('/intro/permission');
+      }
+      if (!this.havePushPermission()) {
+        this.$router.replace('/intro/push');
+      }
+      this.$router.replace('/login');
+    }, 1000);
+  },
+};
+</script>
