c3c46066305c1f7ea165509d66d98f44be7d4b16
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 13:35:09 +0900

    js 스켈레톤 제작

---
+++ project/iitp.infection.pm/assets/res/www/js/ui/IITP_BASIC.js
@@ -0,0 +1,20 @@
+/**
+ * @file : IITP_INTRO_V001.js 인트로
+ * @author :choiyj
+ * @date : 2021.11.30
+ */
+
+
+var _onReady = function(){
+	init.setView();
+	init.setEvent();
+};
+
+var init = {
+	setView : function(){
+
+	},
+	setEvent : function(){
+
+	}
+};
