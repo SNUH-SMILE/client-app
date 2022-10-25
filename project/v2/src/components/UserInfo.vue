6e5d1639634dd9cb12abf548ce1cbeffb9996bf5
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:40:41 +0900

    개인 정보 컴포넌트 개발

---
+++ project/v2/src/components/UserInfo.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="user-info">
+    <div class="user-info-data">
+      <div class="user">
+        <strong class="name">김하나</strong>
+        <span class="lb-txt green">정상</span>
+        <span class="lb-txt red">이탈</span>
+        <span class="lb-txt orange">격리중</span>
+      </div>
+      <ul class="user-info-list">
+        <li><span class="ic-ttl calendar">격리 기간</span> : 2021.07.01 ~ 2021.07.14</li>
+        <li><span class="ic-ttl location">격리 위치</span> : 제1생활치료센터 101호</li>
+      </ul>
+    </div>
+    <p class="user-info-txt">정확한 격리 기간을 확인 후 조회 가능합니다.</p>
+    <p class="user-info-txt none">격리 해제까지 <strong class="iht">7일</strong> 남았습니다.</p>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
