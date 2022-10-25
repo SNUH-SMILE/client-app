68dd160713e5698f00887bd5f2f05d7786385d60
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:36:15 +0900

    체크박스 컴포넌트 개발

---
+++ project/v2/src/common/components/Checkbox.vue
@@ -0,0 +1,28 @@
+<template>
+  <p class="ipt-chk">
+    <input type="checkbox" :name="name" :id="id" :value="value" />
+    <label :for="id">{{ label }}</label>
+  </p>
+</template>
+<script>
+export default {
+  name: 'checkbox',
+  props: {
+    value: {
+      type: Boolean,
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
