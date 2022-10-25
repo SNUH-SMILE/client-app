4a1290932a3b22c23ebc17025850229388513df2
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:58:26 +0900

    toast 플러그인 추가

---
+++ project/v2/src/plugins/modal/components/Toast.vue
@@ -0,0 +1,20 @@
+<template>
+  <transition name="fade" appear v-if="show">
+    <div class="toast-wrap show">
+      <div class="toast-txt" v-html="content"></div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      content: '',
+      show: true,
+    };
+  },
+};
+</script>
+
+<style></style>
