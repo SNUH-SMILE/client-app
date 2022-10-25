f2639bbe9c22f0cec400b39ab33fc43b224fc49b
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:36:22 +0900

    datebox 컴포넌트 개발

---
+++ project/v2/src/common/components/DateBox.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="date-box">
+    <button type="button" class="btn-prev">
+      <span class="txt-blind">전 일</span>
+    </button>
+    <span class="date">{{ date }}</span>
+    <button type="button" class="btn-next">
+      <span class="txt-blind">후 일</span>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    date: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style></style>
