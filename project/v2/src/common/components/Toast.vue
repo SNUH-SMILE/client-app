580fa31e08a17ef5dff90d81602e80f0072ebee7
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:37:06 +0900

    toast 컴포넌트 개발

---
+++ project/v2/src/common/components/Toast.vue
@@ -0,0 +1,15 @@
+<template>
+  <transition name="fade" appear="">
+    <div class="toast-wrap show">
+      <div class="toast-txt">
+        <slot></slot>
+      </div>
+    </div>
+  </transition>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
