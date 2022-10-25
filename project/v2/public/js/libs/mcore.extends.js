db80b817833a025012b6f726899ae3bc34aca5eb
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 15:12:49 +0900

    퍼블릭 파일 추가

---
+++ project/v2/public/js/libs/mcore.extends.js
@@ -0,0 +1,12 @@
+
+(function(window, undefined) {
+
+var thisFileName = "mcore.extends.js",
+
+importFiles = [
+	// "wnInterface.extends.js",
+];
+
+M.ScriptLoader.writeScript( importFiles, M.ScriptLoader.scriptPath(thisFileName) );
+
+})(window);
