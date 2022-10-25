0862dd1f22b6aa6e48fc9aea673c32126fa5e32b
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:40:54 +0900

    기본 레이아웃 컴포넌트 개발

---
+++ project/v2/src/layouts/default.vue
@@ -0,0 +1,17 @@
+<template>
+  <div style="display: contents">
+    <default-header />
+    <keep-alive>
+      <router-view v-if="$route.meta.keepAlive" :key="$route.fullpath"></router-view>
+    </keep-alive>
+    <router-view v-if="!$route.meta.keepAlive" :key="$route.fullpath"></router-view>
+  </div>
+</template>
+<script>
+import DefaultHeader from '@/components/DefaultHeader.vue';
+export default {
+  components: {
+    DefaultHeader,
+  },
+};
+</script>
