ab802f321507a242bc806169232f18062f10733f
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:36:38 +0900

    loading 컴포넌트 개발

---
+++ project/v2/src/common/components/Loading.vue
@@ -0,0 +1,9 @@
+<template>
+  <transition name="fade" appear="">
+    <div class="loading-wrap show">
+      <div class="loading">
+        <img src="@/assets/img/img-loading.png" alt="" />
+      </div>
+    </div>
+  </transition>
+</template>
