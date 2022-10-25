ee6d77af34ad11284977720c79fd8a0876b29b4a
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:38:17 +0900

    문진 작성 알림 컴포넌트 개발

---
+++ project/v2/src/components/HistoryTakingAlarm.vue
@@ -0,0 +1,20 @@
+<template>
+  <modal-layout title="문진 작성 알림">
+    <template slot="body">
+      <p class="pop-txt ic-write">
+        작성하지 않은 문진이 있습니다.
+        <br />시간 내에 반드시 작성해주세요.
+      </p>
+    </template>
+    <template slot="foot">
+      <div class="btn-wrap">
+        <button type="button" class="btn-txt white" @click="$emit('onAction', 'close')">다음에 작성하기</button>
+        <button type="button" class="btn-txt blue" @click="$emit('onAction', 'submit')">지금 작성하기</button>
+      </div>
+    </template>
+  </modal-layout>
+</template>
+
+<script>
+export default {};
+</script>
