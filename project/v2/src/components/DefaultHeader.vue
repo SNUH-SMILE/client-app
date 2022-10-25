591f9f7a68a467a00d1f59d998859f0402aabc10
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:37:55 +0900

    기본 헤더 컴포넌트 개발

---
+++ project/v2/src/components/DefaultHeader.vue
@@ -0,0 +1,61 @@
+<template>
+  <div :class="{ 'header-wrap': showUser }">
+    <header class="header" :class="{ 'no-line': !headerLine }">
+      <h1 class="hd-ttl">{{ headerTitle }}</h1>
+      <button v-if="headerType === 'back'" type="button" class="btn-hd back" @click="$router.go(-1)">
+        <span class="txt-blind">뒤로가기</span>
+      </button>
+      <button v-else-if="headerType === 'close'" type="button" class="btn-hd close" @click="$router.go(-1)">
+        <span class="txt-blind">닫기</span>
+      </button>
+      <template v-else-if="headerType === 'nav'">
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'device' }">
+          <button type="button" class="btn-hd bluetooth" @click="navigate">
+            <span class="txt-blind">기기연결</span>
+          </button>
+        </router-link>
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'alarm' }">
+          <button type="button" class="btn-hd alarm new" @click="navigate">
+            <span class="txt-blind">알림함</span>
+          </button>
+        </router-link>
+        <router-link custom v-slot="{ navigate }" :to="{ name: 'customer' }">
+          <button type="button" class="btn-hd customer" @click="navigate">
+            <span class="txt-blind">고객지원</span>
+          </button>
+        </router-link>
+      </template>
+    </header>
+    <user-info v-if="showUser" />
+  </div>
+</template>
+<script>
+import UserInfo from '@/components/UserInfo.vue';
+export default {
+  name: 'default-header',
+  components: {
+    UserInfo,
+  },
+  data() {
+    return {
+      title: '',
+    };
+  },
+  computed: {
+    headerLine() {
+      return this.$route.meta.headerLine;
+    },
+    headerType() {
+      return this.$route.meta.headerType;
+    },
+    headerTitle() {
+      return this.$route.meta.title;
+    },
+    showUser() {
+      return this.headerType === 'nav';
+    },
+  },
+};
+</script>
+
+<style></style>
