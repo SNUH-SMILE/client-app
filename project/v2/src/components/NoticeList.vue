d57a5b9aa04ae92f31f70a800f476fd5397b0f48
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:40:20 +0900

    공지사항 리스트 컴포넌트 개발

---
+++ project/v2/src/components/NoticeList.vue
@@ -0,0 +1,27 @@
+<template>
+  <ul class="noti-list">
+    <li v-for="(item, index) in notiList" :key="index">
+      <button type="button">
+        <span class="title-box" :class="{ new: item.new }">
+          <span class="ttl" :class="textClass">{{ item.title }}</span>
+          <em class="badge">N</em>
+        </span>
+        <span class="sub-box">
+          <span class="date">{{ item.date }}</span>
+          <span class="point-txt" v-show="item.answer">답변 완료</span>
+        </span>
+      </button>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  props: {
+    notiList: Array,
+    textClass: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
