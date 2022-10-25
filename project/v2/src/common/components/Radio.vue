31972a641dde286cfdc32e61693438c0f56e3cc1
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:36:45 +0900

    radio 컴포넌트 개발

---
+++ project/v2/src/common/components/Radio.vue
@@ -0,0 +1,29 @@
+<template>
+  <p class="ipt-rdo">
+    <input type="radio" :name="name" :id="id" />
+    <label :for="id">{{ label }}</label>
+  </p>
+</template>
+<script>
+export default {
+  name: 'radio',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    id: {
+      type: String,
+      required: true,
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
