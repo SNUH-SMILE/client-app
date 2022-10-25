595c55a0875ef07884cd2cb814658c6dc9b6b7d6
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:37:00 +0900

    textField 컴포넌트 개발

---
+++ project/v2/src/common/components/TextField.vue
@@ -0,0 +1,83 @@
+<template>
+  <div class="form-item">
+    <label :for="id" class="form-ttl">{{ label }}</label>
+    <div class="ipt-wrap" :class="{ disabled }">
+      <textarea v-if="type === 'textarea'" ref="input" v-on="getListeners" v-bind="getInputProps" @input="handleInput" />
+      <input v-else ref="input" v-on="getListeners" v-bind="getInputProps" @input="handleInput" />
+      <span v-if="unit" class="unit">{{ unit }}</span>
+      <button type="button" v-if="reset" class="btn-txt-reset" @click="resetInput">
+        <span class="txt-blind">입력 초기화</span>
+      </button>
+      <slot></slot>
+    </div>
+    <slot name="noti"></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: 'text-field',
+  props: {
+    value: {
+      type: String,
+      default: '',
+    },
+    label: {
+      type: String,
+      default: '',
+    },
+    id: {
+      type: String,
+      default: '',
+    },
+    type: {
+      type: String,
+      default: 'text',
+    },
+    disabled: {
+      type: Boolean,
+      default: false,
+    },
+    readonly: {
+      type: Boolean,
+      default: false,
+    },
+    placeholder: {
+      type: String,
+    },
+    reset: {
+      type: Boolean,
+      default: false,
+    },
+    unit: {
+      type: String,
+      default: '',
+    },
+    inputProps: {
+      type: Object,
+      default() {
+        return {};
+      },
+    },
+  },
+  computed: {
+    getListeners() {
+      /* eslint no-unused-vars: "off" */
+      const { input, ...others } = this.$listeners;
+      return { ...others };
+    },
+    getInputProps() {
+      const { value, reset, unit, inputProps, ...others } = this.$props;
+      return { ...inputProps, ...others };
+    },
+  },
+  methods: {
+    handleInput(e) {
+      this.$emit('input', e.target.value);
+    },
+    resetInput() {
+      this.$refs.input.value = '';
+      this.$emit('input', '');
+    },
+  },
+};
+</script>
