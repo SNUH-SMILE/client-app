9204eac7bcbd8fb8b59b8774e0b1cf88daab8c19
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 15:01:20 +0900

    vue router 추가

---
+++ project/v2/src/router/index.js
@@ -0,0 +1,53 @@
+import Vue from 'vue';
+import Router from 'vue-router';
+import routes from 'vue-auto-routing';
+import { createRouterLayout } from 'vue-router-layout';
+import PageMiddleware from '@/middlewares/page';
+
+Vue.use(Router);
+
+const RouterLayout = createRouterLayout((layout) => {
+  return import('@/layouts/' + layout + '.vue');
+});
+
+// routes.forEach((route) => {
+//   if (route.name === 'home') {
+//     route.components = {
+//       default: route.component,
+//       header: { template: '<h1>test</h1>' },
+//     };
+//     delete route.component;
+//   }
+// });
+console.log(routes);
+
+const router = new Router({
+  mode: 'history',
+  base: process.env.BASE_URL,
+  scrollBehavior(to, from, savePosition) {
+    // scroll 관련 함수
+    // console.log(savePosition);
+  },
+  routes: [
+    {
+      path: '*',
+      redirect: '/',
+    },
+    {
+      path: '/',
+      redirect: '/intro',
+    },
+    {
+      path: '/',
+      component: RouterLayout,
+      children: routes,
+    },
+  ],
+});
+
+router.beforeEach((to, from, next) => {
+  PageMiddleware(to, from);
+  next();
+});
+
+export default router;
