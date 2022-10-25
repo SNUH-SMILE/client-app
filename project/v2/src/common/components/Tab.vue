187f9e4b2689f8c413dc3603689f093d2596aaa4
parkdoyoung <ehdud5578@gmail.com>
2022-10-25 14:36:51 +0900

    tab 컴포넌트 개발

---
+++ project/v2/src/common/components/Tab.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="tab-box">
+    <ul class="tab" :class="scrollClass">
+      <li v-for="(tab, index) in tabs" :key="index" :class="{ selected: currentTab === index }">
+        <button type="button" @click="currentTab = index">{{ tab }}</button>
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    tabs: Array,
+    scrollClass: {
+      type: String,
+      default: '',
+    },
+  },
+  data() {
+    return {
+      currentTab: 0,
+    };
+  },
+};
+</script>
+
+<style></style>
