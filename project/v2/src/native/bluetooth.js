d66f98237c0a569e540450f62e10454ba4250828
yb-k <kyb2206@knou.ac.kr>
2022-11-11 14:34:48 +0900

    feat: buletooth band scan start

--- project/v2/src/native/bluetooth.js
+++ project/v2/src/native/bluetooth.js
@@ -3,3 +3,41 @@
  * BlueTooth 관련
  * 디바이스 연결
  */
+
+import { RUNTIME } from '@/common/config';
+import { APP_ENV } from '@/common/constants';
+import Logger from '@/utils/logger';
+import { extend } from '.';
+
+let _scanCallback = null;
+
+const CB_BAND_SCAN = '__CB_BAND_SACN';
+
+const logger = new Logger('Native Bluetooth');
+
+window[CB_BAND_SCAN] = function (result) {
+  logger.info('CB_BAND_SCAN : ', result);
+  // TODO : 필요한 경우 데이터 핸들러 작성
+  if (typeof _scanCallback === 'function') {
+    _scanCallback(result);
+  } else {
+    logger.warn('unBind Callback : "_scanCallback"');
+  }
+};
+
+export const ON_BAND_SCAN = 'onBandScan';
+extend(ON_BAND_SCAN, (cb) => {
+  _scanCallback = cb;
+  if (RUNTIME.TYPE === APP_ENV.APP) {
+    M.execute('exBandScan', {
+      schBluetooth: 'C',
+      callback: CB_BAND_SCAN,
+    });
+  }
+});
+
+export const OFF_BAND_SCAN = 'offBandScan';
+extend(OFF_BAND_SCAN, () => {
+  _scanCallback = null;
+  M.execute('exBandScanStop');
+});
