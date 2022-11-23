0d35132865bf065c8e31034227718ea9c00add59
parkdoyoung <ehdud5578@gmail.com>
2022-11-23 15:10:22 +0900

    feat : 운동전 문진 추가

---
+++ project/v2/src/views/history-taking/exercise.vue
@@ -0,0 +1,68 @@
+<template>
+  <div class="content-wrap">
+    <validation-observer tag="form" v-slot="{ handleSubmit }">
+      <form @submit.prevent="handleSubmit(onSubmit)">
+        <div class="content">
+          <div class="blue-top-box">
+            <v-step-progress :percent="state.percent" :ing="state.ing" :total="state.total" />
+          </div>
+          <div class="cont-inner mb-space30">
+            <div class="form-box">
+              <v-history-taking-item
+                v-for="(question, index) in amPmQuestion"
+                :key="question.order"
+                :question="question"
+                :index="index + 1"
+              ></v-history-taking-item>
+            </div>
+          </div>
+        </div>
+        <div class="btn-wrap">
+          <button type="button" class="btn-line navy" @click="$router.go(-1)">이전</button>
+          <!-- TODO : user-check 어디로 갈 지 수정해야함. -->
+
+          <button type="submit" class="btn-txt navy">제출</button>
+        </div>
+      </form>
+    </validation-observer>
+  </div>
+</template>
+<route>
+{
+  "meta" : {
+    "title" : "몸 상태 체크하기"
+  }
+}
+</route>
+<script>
+import HistoryModules from '@/modules/history/components';
+import exerciseList from '@/modules/history/json/exerciselist.json';
+
+const INIT_STATE = () => ({
+  ing: 1,
+  total: parseInt(exerciseList.length / 10) + 1,
+  percent: 0,
+});
+
+export default {
+  data() {
+    return {
+      state: INIT_STATE(),
+    };
+  },
+  components: { ...HistoryModules },
+  computed: {
+    amPmQuestion() {
+      return exerciseList;
+    },
+  },
+  methods: {
+    // 메서드 구현
+    onSubmit: function () {
+      console.log('submitted!');
+    },
+  },
+};
+</script>
+
+<style></style>
