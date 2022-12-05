f14cd4a45948246e23e9e1c06d7b26a3262b05b8
yb-k <kyb2206@knou.ac.kr>
2022-12-05 12:14:27 +0900

    feat: 가민 디바이스 연결 추가

--- project/v2/src/views/device/index.vue
+++ project/v2/src/views/device/index.vue
@@ -5,7 +5,7 @@
         <h1 class="ttl-m mb-space30">가지고 계신 기기를 <br /><span class="txtc-blue">연결</span>해 주세요.</h1>
         <ul class="btn-line-list">
           <li>
-            <button type="button" @click="popup = 'garmin'">가민 기기 연결하기</button>
+            <button type="button" @click="openGarminConnect">가민 기기 연결하기</button>
           </li>
           <li>
             <router-link custom v-slot="{ navigate }" :to="{ name: 'device-list' }">
@@ -26,7 +26,6 @@
         <button type="button" class="btn-txt navy" @click="navigate">다음에 연결하기</button>
       </router-link>
     </div>
-    <garmin-connect v-if="popup === 'garmin'" @onClose="popup = ''" />
   </div>
 </template>
 
@@ -38,18 +37,42 @@
 }
 </route>
 <script>
-import GarminConnect from '@/components/GarminConnect.vue';
+import { mapGetters } from 'vuex';
+import { LOGIN_ID } from '@/modules/patient';
+import { OPEN_GARMIN_OAUTH } from '@/native/band';
+import { BIND_RESTORE_EVENT, UNBIND_RESTORE_EVENT } from '@/native/cycle';
+import { PARAM_DATA } from '@/native/data';
 const INIT_STATE = () => ({});
 
 export default {
-  components: {
-    GarminConnect,
-  },
   data() {
     return {
       state: INIT_STATE(),
-      popup: '',
     };
+  },
+  computed: {
+    ...mapGetters({ loginId: LOGIN_ID }),
+  },
+  created() {
+    this.$nativeScript(BIND_RESTORE_EVENT, this.garminConnectResult);
+  },
+  beforeDestroy() {
+    this.$nativeScript(UNBIND_RESTORE_EVENT, this.garminConnectResult);
+  },
+  methods: {
+    garminConnectResult() {
+      const { code, message } = this.$nativeScript(PARAM_DATA, 'params.garminAuthResult');
+      if (code === '200') {
+        this.$alert('가민 연동을 완료하였습니다.');
+      } else {
+        this.$alert(`${message || '가민 연동을 실패하였습니다.'}(${code})`);
+      }
+      this.$nativeScript(PARAM_DATA, 'params', {});
+    },
+    async openGarminConnect() {
+      await this.$alert('가민 연동을 시작합니다.');
+      this.$nativeScript(OPEN_GARMIN_OAUTH, this.loginId, this.loginId);
+    },
   },
 };
 </script>
