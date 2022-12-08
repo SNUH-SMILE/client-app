5d586912ff37a534d8319b8c08f324377493980e
yb-k <kyb2206@knou.ac.kr>
2022-12-08 10:15:06 +0900

    chore: 수면 상세 기본날짜 당일로 수정

--- project/v2/src/views/state/sleep.vue
+++ project/v2/src/views/state/sleep.vue
@@ -105,7 +105,7 @@ export default {
   },
   data() {
     return {
-      date: this.$dayjs('20221125').format('YYYYMMDD'),
+      date: this.$dayjs().format('YYYYMMDD'),
     };
   },
   created() {
