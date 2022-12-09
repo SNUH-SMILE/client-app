5f044f3c00dbca328d6ff537a365c91dfa440da3
parkdoyoung <ehdud5578@gmail.com>
2022-12-09 16:25:40 +0900

    fix : progress bar 적용되게 수정

--- project/v2/src/modules/history/components/VStepProgress.vue
+++ project/v2/src/modules/history/components/VStepProgress.vue
@@ -3,7 +3,7 @@
     <div class="progress-box">
       <!-- 진행률에 따라 width 값을 변경해주세요. -->
       <transition appear @before-enter="beforeStart" @after-enter="start">
-        <div class="bar"></div>
+        <div class="bar" ref="transition"></div>
       </transition>
     </div>
     <div class="txt">STEP {{ ing }}/{{ total }}</div>
@@ -13,11 +13,12 @@
 <script>
 export default {
   name: 'v-step-progress',
+  data() {
+    return {
+      percent: 10,
+    };
+  },
   props: {
-    percent: {
-      type: Number,
-      default: 0,
-    },
     ing: {
       type: Number,
       default: 1,
@@ -25,6 +26,13 @@ export default {
     total: {
       type: Number,
       default: 2,
+    },
+  },
+  watch: {
+    ing: function (newValue) {
+      this.percent = parseInt((newValue / this.total) * 100);
+      this.$refs.transition.style.width = `${this.percent}%`;
+      this.$refs.transition.style.transition = `width 1s linear`;
     },
   },
   methods: {
