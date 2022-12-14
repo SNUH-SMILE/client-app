6de24b13c129a63ef8eac4ea6096a72d648c16fb
yb-k <kyb2206@knou.ac.kr>
2022-12-14 10:52:51 +0900

    feat: Top 버튼 기능

--- project/v2/src/common/components/TopButton.vue
+++ project/v2/src/common/components/TopButton.vue
@@ -1,5 +1,14 @@
 <template>
-  <button type="button" class="btn-top">
+  <button type="button" class="btn-top" @click="moveTopScroll">
     <span class="txt-blind">맨위로</span>
   </button>
 </template>
+<script>
+export default {
+  methods: {
+    moveTopScroll() {
+      document.querySelector('.content').scrollTop = 0;
+    },
+  },
+};
+</script>
