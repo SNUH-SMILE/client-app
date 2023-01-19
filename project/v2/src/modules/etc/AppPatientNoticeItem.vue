7f7872630c5f4145d64eecfb34a33953134de268
yb-k <kyb2206@knou.ac.kr>
2023-01-19 11:49:47 +0900

    feat: 사용자 개별 알림 개선사항 대응

---
+++ project/v2/src/modules/etc/AppPatientNoticeItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>
+    <button type="button" @click="$emit('click')">
+      <span class="title-box" :class="{ isNew }">
+        <pre class="ttl" :class="{ ellipsis: !showContent }">{{ title }}</pre>
+        <em class="badge">N</em>
+      </span>
+      <span class="sub-box">
+        <span class="date">{{ date }}</span>
+      </span>
+    </button>
+  </li>
+</template>
+
+<script>
+export default {
+  props: {
+    isNew: {
+      type: Boolean,
+      default: false,
+    },
+    ellipsis: {
+      type: Boolean,
+      default: true,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+    date: {
+      type: String,
+      default: '',
+    },
+    showContent: {
+      type: Boolean,
+      default: false,
+    },
+  },
+};
+</script>
+
+<style></style>
--- project/v2/src/modules/etc/AppPatientNoticeList.vue
+++ project/v2/src/modules/etc/AppPatientNoticeList.vue
@@ -2,21 +2,28 @@
   <app-notice-list class="tb-icon-none">
     <app-notice-item
       v-for="(item, index) in list"
-      @click="onClickItem(item)"
+      @click="onClickItem(item, index)"
       :key="`patient-notice-${index}`"
+      :ref="`item${index}`"
       :title="item.noticeBody"
       :date="item.noticeDateLabel"
+      :showContent="selectedItem === item"
     />
   </app-notice-list>
 </template>
 <script>
 import { mapActions, mapGetters } from 'vuex';
 import AppNoticeList from '@/components/AppNoticeList.vue';
-import AppNoticeItem from '@/components/AppNoticeItem.vue';
+import AppNoticeItem from './AppPatientNoticeItem.vue';
 import { PATIENT_NOTICE_LIST } from '@/modules/etc';
 
 export default {
   components: { AppNoticeList, AppNoticeItem },
+  data() {
+    return {
+      selectedItem: null,
+    };
+  },
   async created() {
     await this.fetchList();
   },
@@ -30,8 +37,16 @@ export default {
     ...mapActions({
       fetchList: PATIENT_NOTICE_LIST,
     }),
-    onClickItem(item) {
-      this.$emit('selectedItem', item);
+    onClickItem(item, index) {
+      this.selectedItem = this.selectedItem === item ? null : item;
+      const $target = this.$refs[`item${index}`];
+      if ($target[0].$el) {
+        this.$nextTick(() => {
+          setTimeout(() => {
+            $target[0].$el.scrollIntoView({ block: 'start', behavior: 'smooth' });
+          }, 100);
+        });
+      }
     },
   },
 };
