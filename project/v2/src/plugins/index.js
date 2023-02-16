2c100c2375abf85ee6fb28e9544fb1c9be02ecaa
yb-k <kyb2206@knou.ac.kr>
2023-02-16 16:42:14 +0900

    fix: file프로토콜에서 주소검색 안되는 이슈 수정

--- project/v2/src/plugins/index.js
+++ project/v2/src/plugins/index.js
@@ -8,5 +8,5 @@ import ModalPlugin from './modal';
 Vue.use(Fragment.Plugin);
 Vue.use(ModalPlugin);
 Vue.use(AppPlugin);
-Vue.use(VueDaumPostcode);
+Vue.use(VueDaumPostcode, { scriptUrl: 'http://t1.daumcdn.net/mapjsapi/bundle/postcode/prod/postcode.v2.js' });
 Vue.use(VueYoutube);
